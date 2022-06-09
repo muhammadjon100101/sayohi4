265bae71243e09bfaf4bb97022735d47ab960f8e
Muhammadjon Hasanov <muhammadjon100101@gmail.com>
2022-06-09 12:54:55 +0300

    some changes

--- src/app/explore-page/explore-page.component.ts
+++ src/app/explore-page/explore-page.component.ts
@@ -7,6 +7,16 @@ import { OwlOptions } from 'ngx-owl-carousel-o';
   styleUrls: ['./explore-page.component.scss']
 })
 export class ExplorePageComponent implements OnInit { 
+  exploreBlock:any = [
+    {id:1,img:'./assets/img/person2.svg',avatar:'./assets/img/avatarPerson.svg', name:'Muhamamdjon',byName:'Okil'},
+    {id:2,img:'./assets/img/person2.svg',avatar:'./assets/img/avatarPerson.svg', name:'Mustafojon',byName:'Okil'},
+    {id:3,img:'./assets/img/person2.svg',avatar:'./assets/img/avatarPerson.svg', name:'Umar',byName:'Okil'},
+    {id:4,img:'./assets/img/person2.svg',avatar:'./assets/img/avatarPerson.svg', name:'Usmon',byName:'Okil'},
+    {id:5,img:'./assets/img/person2.svg',avatar:'./assets/img/avatarPerson.svg', name:'Ali',byName:'Okil'},
+    {id:6,img:'./assets/img/person2.svg',avatar:'./assets/img/avatarPerson.svg', name:'Ahmad',byName:'Okil'},
+  ]
+  
+  
   constructor() { }
 
   ngOnInit(): void {
